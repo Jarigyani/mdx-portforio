31a81e0e3143745797f75cd29711287df4180356
Jarigyani <coffee.mill.jp@gmail.com>
2023-01-14 17:38:46 +0900

    Add: @next/font導入してフォント変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,17 @@ import Footer from '@/Footer'
 import MDXComponents from '@/MDX/MDXComponents'
 import Navbar from '@/Navbar'
 import { MDXProvider } from '@mdx-js/react'
+import { Noto_Serif_JP } from '@next/font/google'
 import { AnimatePresence } from 'framer-motion'
 import type { AppProps } from 'next/app'
 import { useStore } from 'store'
 import '../styles/globals.css'
+
+const inter = Noto_Serif_JP({
+  weight: '400',
+  subsets: ['japanese'],
+  variable: '--font-inter',
+})
 
 export default function App({ Component, pageProps, router }: AppProps) {
   const darkMode = useStore((state) => state.darkMode)
@@ -17,6 +24,11 @@ export default function App({ Component, pageProps, router }: AppProps) {
         className="min-h-screen w-screen"
         data-theme={darkMode ? `mytheme` : `light`}
       >
+        <style jsx global>{`
+          html {
+            font-family: ${inter.style.fontFamily}, serif;
+          }
+        `}</style>
         <Navbar />
         <MDXProvider components={components}>
           <AnimatePresence mode="wait">
