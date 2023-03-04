9952b05ba01711a8e439ff922027a7305b669b36
Jarigyani <coffee.mill.jp@gmail.com>
2023-03-04 12:59:38 +0900

    feat: OGPのテスト

--- components/MDX/GetOGP.ts
+++ components/MDX/GetOGP.ts
@@ -2,7 +2,6 @@ export const getOGP = async (url: string) => {
   try {
     const res = await fetch(`jariogp.jp/?url=${url}`)
     const data = await res.json()
-    console.log(data)
     return data
   } catch (err) {
     console.error(err)
