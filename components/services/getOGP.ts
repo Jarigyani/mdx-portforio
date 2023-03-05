ade9c5a61bd58a6db3ebc831357e79203fc9df7b
Jarigyani <coffee.mill.jp@gmail.com>
2023-03-06 01:02:03 +0900

    feat: 記事編集

--- components/services/getOGP.ts
+++ components/services/getOGP.ts
@@ -1,6 +1,8 @@
 export const getOGP = (url: string) => {
   const res = fetch(
-    `https://go-http-function-3ck3ffauga-an.a.run.app?url=${url}`
+    `https://go-http-function-3ck3ffauga-an.a.run.app/ogp?url=${url}&appid=${
+      process.env.NEXT_PUBLIC_APP_ID || ''
+    }`
   ).then((res) => res.json())
   return res
 }
